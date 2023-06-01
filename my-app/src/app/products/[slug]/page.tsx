21647b70b6568b81cdac3d98e2368e6a9f9688a6
agratos318 <agratos318@naver.com>
2023-06-01 13:22:15 +0900

    5강 공부중

--- my-app/src/app/products/[slug]/page.tsx
+++ my-app/src/app/products/[slug]/page.tsx
@@ -1,6 +1,10 @@
 import { Metadata } from 'next';
 import { notFound } from 'next/navigation';
 
+import { getProduct, getProducts } from '@/service/products';
+
+export const revalidate = 10;
+
 type Props = {
   params: {
     slug: string;
@@ -13,16 +17,19 @@ export function generateMetadata({ params }: Props) {
   }
 }
 
-export default function PantsPage({ params }: Props) {
-  if (params.slug === 'nothing') {
+export default async function ProductPage({ params: {slug} }: Props) {
+  const product = await getProduct(slug)
+
+  if(!product) {
     notFound();
   }
-  return <h1>{params.slug} 제품 설명 페이지</h1>;
+
+  return <h1>{product.name} 제품 설명 페이지</h1>;
 }
 
-export function generateStaticParams() {
-  const products = ['pants', 'skirt'];
+export async function generateStaticParams() {
+  const products = await getProducts();
   return products.map((product) => ({
-    slug: product,
+    slug: product.id,
   }));
 }
--- my-app/src/app/products/page.tsx
+++ my-app/src/app/products/page.tsx
@@ -1,18 +1,24 @@
+import { getProducts } from '@/service/products';
 import Link from 'next/link';
 
-const products = ['shirt', 'pants', 'skirt', 'shoes'];
+import MeowArticle from '@/components/MeowArticle';
 
-export default function ProductsPage() {
+//export const revalidate = 3;
+
+export default async function ProductsPage() {
+  const products = await getProducts();
+
   return (
     <>
       <h1>제품 소개 페이지!</h1>
       <ul>
         {products.map((product, index) => (
           <li key={index}>
-            <Link href={`/products/${product}`}>{product}</Link>
+            <Link href={`/products/${product.id}`}>{product.name}</Link>
           </li>
         ))}
       </ul>
+      <MeowArticle />
     </>
   );
 }
